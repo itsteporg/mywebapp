40bf840b419bf440c0aa63a4f9a6a08e8a1dc9ed
stas <prykhodko.s.i@gmail.com>
2017-03-27 20:56:23 +0300

    second 27.03

---
+++ src/main/java/org/itstep/myWebApp/repository/datajpa/DataJpaRepository.java
@@ -0,0 +1,39 @@
+package org.itstep.myWebApp.repository.datajpa;
+
+import org.itstep.myWebApp.model.User;
+import org.itstep.myWebApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by student on 27.03.2017.
+ */
+@Repository
+public class DataJpaRepository implements UserRepository {
+
+    @Autowired
+    ProxyRepository repo;
+
+    @Override
+    public List getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return repo.deleteById(id) > 0 ? true:false;
+    }
+
+    @Override
+    public User save(User user) {
+        return repo.save(user);
+    }
+
+    @Override
+    public User getById(Integer id) {
+        return repo.findOne(id);
+    }
+}
---
+++ src/main/java/org/itstep/myWebApp/repository/datajpa/ProxyRepository.java
@@ -0,0 +1,13 @@
+package org.itstep.myWebApp.repository.datajpa;
+
+import org.itstep.myWebApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by student on 27.03.2017.
+ */
+public interface ProxyRepository extends JpaRepository<User, Integer> {
+
+    int deleteById(Integer id);
+
+}
