d33c8fa6897b3269e61aa231d837a3b9b6b4ccf1
stas <prykhodko.s.i@gmail.com>
2017-04-03 21:05:19 +0300

    third 03.04

--- src/main/java/org/itstep/myWebApp/service/UserService.java
+++ src/main/java/org/itstep/myWebApp/service/UserService.java
@@ -4,6 +4,8 @@ import org.itstep.myWebApp.model.User;
 import org.itstep.myWebApp.repository.UserRepository;
 import org.itstep.myWebApp.util.ExceptionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,19 +16,26 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Cacheable(value = "cache1")
     public List<User> getAll() {
         return repository.getAll();
     }
 
+
+    @CacheEvict(value = "cache1", allEntries = true)
     public void delete(Integer id) {
         ExceptionUtil.check(repository.getById(id), id);
         repository.delete(id);
     }
 
+
+    @CacheEvict(value = "cache1", allEntries = true)
     public User save(User user) {
         return repository.save(user);
     }
 
+
+    @CacheEvict(value = "cache1", allEntries = true)
     public void update(User user) {
         ExceptionUtil.check(repository.getById(user.getId()), user.getId());
         repository.save(user);
